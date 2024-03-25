cdab0ac4f2ed1de13514a22ed331f1882ef5bf0b
Earthlight <earthlight2187@hotmail.com>
2024-03-25 09:52:26 +0800

    update examples

--- Examples/demo.py
+++ Examples/demo.py
@@ -7,6 +7,6 @@ read_path: PathLike = "./src_data.xlsx"
 save_path: PathLike = "./output.xlsx"
 
 src_db: Database = Database.import_from_xlsx(read_path)
-write_db = mt.execute_migration("./MigrationConfig3.rjson", src_db, [], ["MSA", 'D'])
+write_db = mt.execute_migration("./DemoConfig.rjson", src_db, args=["20230601"])
 
-write_db.export_to_xlsx(save_path, False)
+write_db.export_to_xlsx(save_path)
